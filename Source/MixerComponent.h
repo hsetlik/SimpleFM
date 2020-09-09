39d7a743e72289a5fb7889988dd6b18e956827b8
Hayden Setlik <haydensetlik@gmail.com>
2020-09-09 14:48:47 -0600

    added mixer files

---
+++ Source/MixerComponent.h
@@ -0,0 +1,17 @@
+/*
+  ==============================================================================
+
+    MixerComponent.h
+    Created: 9 Sep 2020 2:45:39pm
+    Author:  Hayden Setlik
+
+  ==============================================================================
+*/
+
+#pragma once
+#include <JuceHeader.h>
+
+class MixerComponent : public juce::Component, public juce::Slider::Listener
+{
+    
+}
